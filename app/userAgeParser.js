c4fde008a511de18631f4771f46cbf869fc4648a
tzorca <tzorca@users.noreply.github.com>
2017-02-02 23:24:07 -0500

    Add comments

--- app/userAgeParser.js
+++ app/userAgeParser.js
@@ -1,11 +1,14 @@
+// Parse an array of lines into an object map containing number of users for each distinct age.
+// Each line should contain userID and userAge delimited by a comma.
 module.exports.parseLinesToUsersByAge = function(lines) {
     var usersByAge = {};
-    lines.forEach(function(line){
+    lines.forEach(function(line) {
         var fields = line.split(/,/);
         if (fields.length != 2) {
             console.log('Invalid number of fields on line');
             return;
         }
+
         var userID = fields[0];
         var userAge = fields[1];
         if (!(userAge in usersByAge)) {
--- index.js
+++ index.js
@@ -6,13 +6,17 @@ var userAgeParser = require('./app/userAgeParser.js');
 
 
 if (cluster.isMaster) {
+    // Begin timing application.
     console.time('app');
-    if (process.argv.length < 3) {
-        return console.log("Please include a filename parameter.");
-    }
 
+    // First program parameter should be a path to the file that will be processed.
+    if (process.argv.length < 3) {
+        return console.log("Please include a filename as the first parameter.");
+    }
     var filename = process.argv[2];
 
+    // An optional second parameter of 'singlecore' will allow the app to run in single core mode.
+    // This can be used to compare performance between multi-core and single-core mode.
     var multicore = true;
     if (process.argv.length >= 4) {
         if (process.argv[3] == 'singlecore') {
@@ -20,72 +24,98 @@ if (cluster.isMaster) {
         }
     }
 
-    fs.readFile(filename, 'utf8', function(err, data) {
+    // Read the file.
+    fs.readFile(filename, 'utf8', function(err, fileData) {
         if (err) {
             return console.log(err);
         }
 
-        var lines = splitIntoLines(data);
+        // Split the file data into lines for processing.
+        var lines = splitIntoLines(fileData);
 
         if (multicore) {
-           coordinateWorkers(lines);
+            // In multicore mode, coordinate workers to work in line chunks.
+            coordinateWorkers(lines, function onCompletion(subResults) {
+
+                // After all workers are finished, merge their results together.
+                var usersByAge = mergeObjectsAddValues(subResults);
+
+                // Log the results.
+                logUsersByAge(usersByAge);
+
+                // Time app completion.
+                console.timeEnd('app');
+            });
         } else {
+            // In single-core mode, just parse all the lines and log the results.
             var usersByAge = userAgeParser.parseLinesToUsersByAge(lines);
             logUsersByAge(usersByAge);
-        }
 
+            // Time app completion.
+            console.timeEnd('app');
+        }
     });
+
 } else if (cluster.isWorker) {
+    // A worker process will receive lines from the master process.
     process.on('message', function(lines) {
+        // Lines are parsed into a users by age map, then sent back to the master.
         var usersByAge = userAgeParser.parseLinesToUsersByAge(lines);
         process.send(usersByAge);
     });
 }
 
 
+// Splits text into an array of lines.
 function splitIntoLines(text) {
     return text.match(/[^\r\n]+/g);
 }
 
 
-function coordinateWorkers(lines) {
+// Coordinates workers to to work on chunked elements of the lines array.
+// Calls onCompletion function when all workers are finished.
+function coordinateWorkers(lines, onCompletion) {
     var cpuCount = os.cpus().length;
     var subResults = [];
+    var completedWorkerCount = 0;
 
+    // Split the lines into chunks that can be processed by each worker.
     var chunkedLines = chunkifier.chunkify(lines, cpuCount);
 
-    var completedWorkerCount = 0;
     for (var i = 0; i < cpuCount; i++) {
+
+        // For each core/CPU, create a new process in worker mode.
         var worker = cluster.fork();
+
+        // Send the lines to be processed to the new worker.
         worker.send(chunkedLines[i]);
 
         worker.on('message', function workerFinished(subResult) {
-            // This worker is done
+            // When a worker sends a message to the master, that worker has finished.
             worker.kill();
             completedWorkerCount++;
-
             subResults.push(subResult);
 
+            // If all the workers are finished, run the completion function.
             if (completedWorkerCount == cpuCount) {
-                var usersByAge = mergeObjectsAddValues(subResults);
-                logUsersByAge(usersByAge);
+                onCompletion(subResults);
             }
         });
     }
 }
 
 
+// Merges together multiple objects with numeric values.
+// If the objects have the same keys, adds their values together in the result object.
 function mergeObjectsAddValues(objs) {
     var result = {};
 
     objs.forEach(function(obj) {
         Object.keys(obj).forEach(function(key) {
-            result[key] = 0;
-        });
-    });
+            if (!(key in result)) {
+                result[key] = 0;
+            }
 
-    objs.forEach(function(obj) {
-        Object.keys(obj).forEach(function(key) {
             result[key] += obj[key];
         });
     });
@@ -94,11 +124,10 @@ function mergeObjectsAddValues(objs) {
 }
 
 
+// Outputs a list of tuples of distinct age and count of users with that age.
 function logUsersByAge(usersByAge) {
     Object.keys(usersByAge).forEach(function(age) {
         var userCount = usersByAge[age];
         console.log([age, userCount]);
     });
-
-    console.timeEnd('app');
 }
