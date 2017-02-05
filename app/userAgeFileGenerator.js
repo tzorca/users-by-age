fa04aa868d2ea649c0eb32abe60040388e332952
tzorca <tzorca@users.noreply.github.com>
2017-02-05 16:11:13 -0500

    Update userAgeFileGenerator to not generate strings with duplicate user IDs

--- app/userAgeFileGenerator.js
+++ app/userAgeFileGenerator.js
@@ -1,13 +1,23 @@
+// Generates a test string for a file in the format read by userAgeParser.
 module.exports.generate = function(minAge, maxAge, userCount) {
     var output = '';
+    
+    var userIDs = [];
+    for (var i = 1; i <= userCount; i++) {
+        userIDs.push(i);
+    }
+
+    shuffle(userIDs);
+
     for (var i = 0; i < userCount; i++) {
         var age = getRandomInt(minAge, maxAge);
-        var userID = getRandomInt(1, 1000000);
+        var userID = userIDs.pop();
         output += userID + ',' + age + '\r\n';
     }
 
     return output;
 };
+
 
 /**
  * Sourced from http://stackoverflow.com/a/1527820
@@ -17,3 +27,20 @@ module.exports.generate = function(minAge, maxAge, userCount) {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+/**
+ * Sourced from http://stackoverflow.com/a/6274381
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
